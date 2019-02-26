5bd9fc8deb26701e228f971cd185f1b7de9b4dff
Markus Helminen <markus.helminen@outlook.com>
2019-02-26 16:42:54 +0200

    state & conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return (
-    <div>
-      Hi there!
-      <SeasonDisplay />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({ lat: position.coords.latitude });
+      },
+      error => {
+        console.log(error);
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+
+  render() {
+    return this.state.errorMessage && !this.state.lat ? (
+      <div>Error: {this.state.errorMessage}</div>
+    ) : !this.state.errorMessage && this.state.lat ? (
+      <div>Latitude: {this.state.lat}</div>
+    ) : (
+      <div>Loading!</div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
